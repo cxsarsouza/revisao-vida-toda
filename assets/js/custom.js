11d8066a8d4b5df765a9394f781637764997a8ff
cxsarsouza <99675804+cxsarsouza@users.noreply.github.com>
2023-01-16 16:34:18 -0300

    mudanças

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -105,3 +105,17 @@ details.forEach((targetDetail) => {
     });
   });
 });
+
+(function ($) {
+
+    /********  wow.js *******/
+    var wow = new WOW({
+        boxClass: 'wow', // animated element css class (default is wow)
+        animateClass: 'animated', // animation css class (default is animated)
+        offset: 50, // distance to the element when triggering the animation (default is 0) 
+        mobile: false
+      });
+      wow.init();
+      
+  })(jQuery);
+  
